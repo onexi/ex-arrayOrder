2c4d90262f8493170908dc221bca7a033d9e783f
sdrammis <sdrammis@mit.edu>
2018-02-11 22:05:36 -0500

    Updated exercise file(s)

--- sdrammis/exercise.js
+++ sdrammis/exercise.js
@@ -4,9 +4,7 @@ exercise.order = function(unordered) {
     var ordered = [];
 
     // Sorting a numerical array from minimum to maximum
-    console.log('testing...');
-
-    return ordered;
+    return ordered.sort((a, b) => a - b);
 };
 
 var unordered = [11, 2, 15, 4, 5, 20, 7, 17, 13, 10, 1, 12, 9, 14, 3, 16, 8, 18, 19, 6];
